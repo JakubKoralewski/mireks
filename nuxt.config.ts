75fd5832dcad4acce79e1cd2721b79342f500a46
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-06-24 21:36:37 +0200

    Schemas seemingly all fine.
    #TODO: Add image JSON-LD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,86 @@
 import NuxtConfiguration from "@nuxt/config";
 // tslint:disable-next-line: no-var-requires
 const pkg = require("./package");
+import services from "./components/ServicesSearch/services";
+
+const offerItemListElement: any[] = [];
+
+for (const service of services) {
+	offerItemListElement.push({
+		"@type": "Offer",
+		itemOffered: {
+			"@type": "Service",
+			name: service.title,
+			alternateName: service.shortTitle,
+			serviceType: "Usługa rachunkowa",
+			provider: {
+				"@type": "Organization",
+				legalName: "Firma Handlowo-Usługowa Mireks Mirosław Koralewski",
+				name: "Mireks",
+				alternateName: "FHU MIREKS"
+			}
+		}
+	});
+}
+
+const structuredJSONData = {
+	"@context": "http://schema.org",
+	"@type": "AccountingService",
+	name: "Mireks",
+	address: {
+		"@type": "PostalAddress",
+		streetAddress: "Nakwasińska 1",
+		addressLocality: "Koźminek",
+		addressRegion: "Wielkopolska",
+		postalCode: "62-840"
+	},
+	currenciesAccepted: "PLN",
+	email: "mireks40@poczta.onet.pl",
+	telePhone: "+48 62 763 74 10",
+	paymentAccepted: ["cash", "check", "credit card"],
+	openingHours: "Mo,Tu,We,Th,Fr 08:00-16:00",
+	openingHoursSpecification: [
+		{
+			"@type": "OpeningHoursSpecification",
+			dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+			opens: "08:00",
+			closes: "16:00"
+		}
+	],
+	geo: {
+		"@type": "GeoCoordinates",
+		latitude: "51.7956549,",
+		longitude: "18.336128"
+	},
+	priceRange: "$",
+	hasOfferCatalog: {
+		"@type": "OfferCatalog",
+		name: "Usługi",
+		itemListElement: offerItemListElement
+	}
+};
+
+// schemaName: "availableChannel",
+// 		schemaInfo: {
+// 			"@type": "ServiceChannel",
+// 			name: "Mireks",
+// 			availableLanguage: {
+// 				"@type": "Language",
+// 				name: "Polish",
+// 				alternateName: "pl"
+// 			},
+// 			serviceLocation: {
+// 				type: "AccountingService",
+// 				name: "Mireks",
+// 				address: {
+// 					"@type": "PostalAddress",
+// 					streetAddress: "Nakwasińska 1",
+// 					addressLocality: "Koźminek",
+// 					addressRegion: "Wielkopolska",
+// 					postalCode: "62-840"
+// 				}
+// 			}
+// 		}
 
 const config: NuxtConfiguration = {
 	mode: "universal",
@@ -22,6 +102,13 @@ const config: NuxtConfiguration = {
 				content: pkg.description
 			}
 		],
+		script: [
+			{
+				type: "application/ld+json",
+				innerHTML: JSON.stringify(structuredJSONData)
+			}
+		],
+		__dangerouslyDisableSanitizers: ["script"],
 		link: [
 			{
 				rel: "icon",
