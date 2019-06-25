922c311d97fd51092410bb81a3e7816bc5bfc59d
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-06-25 13:25:31 +0200

    Merge Schemas (#4) (squash)
    * Add services schemas.
    
    * Schemas seemingly all fine.
    
    #TODO: Add image JSON-LD
    
    * Allow robots.
    
    * Move mireks_bg.jpg to static.
    
    * Contacts.ts: Miscellaneous.
    
    * Add more schemas to JSON-LD.
    
    * Optimize images
    
    * Remove default favicon.
    
    * Add browserconfig.xml for Microsoft Tiles.
    
    * More meta tags!
    
    * Disappear buttons and logo at start.
    
    * Footer: center a#credits.
    
    * Move description to one place and add URL.
    
    * Miscellaneous.

--- components/ServicesSearch/Service/Service.vue
+++ components/ServicesSearch/Service/Service.vue
@@ -1,14 +1,21 @@
 <template>
-	<div :class="[{visible: isVisible(searchText)}, 'service']">
+	<div
+	 :class="[{visible: isVisible(searchText)}, 'service']"
+	 itemprop="itemOffered"
+	 itemscope
+	 itemtype="http://schema.org/Service"
+	>
 		<div
 		 class="title"
 		 :style="`--length: ${50/(service.shortTitle ? service.shortTitle : service.title).length + 10}`"
+		 itemprop="name"
 		>
 			{{service.shortTitle ? service.shortTitle : service.title}}
 		</div>
 		<div
 		 class="description"
 		 :style="`--length: ${100/service.description.length + 7}`"
+		 itemprop="description"
 		>
 			{{service.description}}
 		</div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,119 @@
 import NuxtConfiguration from "@nuxt/config";
 // tslint:disable-next-line: no-var-requires
 const pkg = require("./package");
+import services from "./components/ServicesSearch/services";
+
+const offerItemListElement: any[] = [];
+
+for (const service of services) {
+	offerItemListElement.push({
+		"@type": "Offer",
+		itemOffered: {
+			"@type": "Service",
+			name: service.title,
+			alternateName: service.shortTitle,
+			serviceType: "Usługa rachunkowa",
+			provider: {
+				"@id": "mireks_org"
+			},
+			availableChannel: {
+				"@type": "ServiceChannel",
+				availableLanguage: {
+					"@type": "Language",
+					name: "Polish",
+					alternateName: "pl"
+				},
+				serviceLocation: {
+					"@id": "mireks_place"
+				},
+				servicePhone: "+48 62 763 74 10",
+				servicePostalAddress: {
+					"@id": "address"
+				}
+			}
+		}
+	});
+}
+
+const structuredJSONData = {
+	"@context": "http://schema.org",
+	"@graph": [
+		{
+			"@context": "http://schema.org",
+			"@id": "mireks_org",
+			"@type": "Organization",
+			legalName: "Firma Handlowo-Usługowa Mireks Mirosław Koralewski",
+			name: "Mireks",
+			alternateName: "FHU MIREKS"
+		},
+		{
+			"@id": "mireks_place",
+			"@type": "Place",
+			address: {
+				"@id": "address"
+			}
+		},
+		{
+			"@id": "address",
+			"@type": "PostalAddress",
+			streetAddress: "Nakwasińska 1",
+			addressLocality: "Koźminek",
+			addressRegion: "Wielkopolska",
+			postalCode: "62-840",
+			addressCountry: "Poland"
+		}
+	],
+	"@type": "AccountingService",
+	name: "Mireks",
+	contactPoint: {
+		"@type": "ContactPoint",
+		availableLanguage: {
+			"@type": "Language",
+			name: "Polish",
+			alternateName: "pl"
+		},
+		contactType: "Customer service",
+		email: "mireks40@poczta.onet.pl",
+		telephone: "+48 62 763 74 10",
+		areaServed: "Poland"
+	},
+	address: {
+		"@id": "address"
+	},
+	photo: "/mireks_bg.jpg",
+	image: "/mireks_og.jpg",
+	currenciesAccepted: "PLN",
+	email: "mireks40@poczta.onet.pl",
+	telePhone: "+48 62 763 74 10",
+	paymentAccepted: ["cash", "check", "credit card"],
+	openingHours: "Mo,Tu,We,Th,Fr 08:00-16:00",
+	openingHoursSpecification: [
+		{
+			"@type": "OpeningHoursSpecification",
+			dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+			opens: "08:00",
+			closes: "16:00"
+		}
+	],
+	geo: {
+		"@type": "GeoCoordinates",
+		latitude: "51.7956549,",
+		longitude: "18.336128"
+	},
+	priceRange: "$",
+	hasOfferCatalog: {
+		"@type": "OfferCatalog",
+		name: "Usługi",
+		itemListElement: offerItemListElement
+	}
+};
+
+const TITLE = "FHU Mireks - Biuro Rachunkowe";
+const KEYWORDS =
+	"biuro rachunkowe, księgowy, księgowa, koźminek, mirosław koralewski, mireks, fhu, mirex, kalisz, polska, biuro";
+const DESCRIPTION =
+	"Strona internetowa biura rachunkowego FHU Mireks znajdującego się w Koźminku (k. Kalisza) na ulicy Nakwasińskiej 1.";
+const URL = "https://fhumireks.pl";
 
 const config: NuxtConfiguration = {
 	mode: "universal",
@@ -9,7 +122,16 @@ const config: NuxtConfiguration = {
 	** Headers of the page
 	*/
 	head: {
-		title: pkg.name,
+		title: TITLE,
+		noscript: [
+			{
+				innerHTML:
+					"Ta strona wymaga włączenia JavaScript do prawidłowego działania!"
+			}
+		],
+		htmlAttrs: {
+			lang: "pl"
+		},
 		meta: [
 			{ charset: "utf-8" },
 			{
@@ -19,14 +141,95 @@ const config: NuxtConfiguration = {
 			{
 				hid: "description",
 				name: "description",
-				content: pkg.description
+				content: DESCRIPTION
+			},
+			{
+				hid: "keywords",
+				name: "keywords",
+				content: KEYWORDS
+			},
+			{
+				name: "keyword",
+				content: KEYWORDS
+			},
+			{
+				name: "msapplication-TileColor",
+				content: "#0a1020"
+			},
+			{
+				name: "msapplication-TileImage",
+				content: "/icon192.png"
+			},
+			{
+				name: "msapplication-config",
+				content: "/browserconfig.xml"
+			},
+			{
+				name: "theme-color",
+				content: "#0a1020"
+			},
+			{
+				name: "og:image",
+				content: "/mireks_og.jpg"
+			},
+			{
+				name: "og:url",
+				content: URL
+			},
+			{
+				name: "og:title",
+				content: TITLE
+			},
+			{
+				name: "og:description",
+				content: DESCRIPTION
+			},
+			{
+				name: "fb:app_id",
+				content: "331204421139475"
+			},
+			{
+				name: "twitter:description",
+				content: DESCRIPTION
+			},
+			{
+				name: "twitter:title",
+				content: TITLE
+			},
+			{
+				name: "twitter:image",
+				content: "/mireks_og.jpg"
+			},
+			{
+				name: "twitter:card",
+				content: "summary_large_image"
 			}
 		],
+		script: [
+			{
+				type: "application/ld+json",
+				innerHTML: JSON.stringify(structuredJSONData)
+			}
+		],
+		__dangerouslyDisableSanitizers: ["script"],
 		link: [
 			{
 				rel: "icon",
-				type: "image/x-icon",
-				href: "/favicon.ico"
+				type: "image/png",
+				href: "/icon16.png",
+				sizes: "16x16"
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/icon192.png",
+				sizes: "192x192"
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/icon32.png",
+				sizes: "32x32"
 			},
 			{
 				rel: "stylesheet",
