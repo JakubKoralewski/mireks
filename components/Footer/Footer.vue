aae8b88e0b9e95ee25819b427e76e9d5b4bb4bbf
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-06-27 20:52:17 +0200

    Footer redesign (#19)
    * Some orange accents.
    
    * Make loading orange
    
    * Make the orange map outline smaller.
    
    * Add slight gradient in Footer.
    
    * Footer more orange.
    
    * Fix the map borders.
    
    * Footer coloring.
    
    * Footer display: flex.
    
    * Footer flex complete!
    
    ![](https://streamable.com/rl4m9)

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -2,50 +2,59 @@
 	<section id="footer">
 		<div id="footer-container">
 
-			<div id="logo">
+			<div id="logotype">
 				<img
 				 svg-inline
 				 src="@/other_assets/logo.svg"
 				 class="logo"
 				 ref="logo"
 				/>
+				<span id="description">
+					Firma Handlowo-Usługowa<br /> Mireks Mirosław Koralewski
+				</span>
 			</div>
-			<div id="description">
-				Firma Handlowo-Usługowa Mireks Mirosław Koralewski
+			<div id="middle">
+
+				<div id="nip-regon">
+					<b>NIP:</b>
+					6181009277<br />
+					<b>REGON:</b>
+					250454919<br />
+					<b>Świadectwo kwalfikacyne:</b>
+					nr 20113/2000
+				</div>
+				<div id="business-time">
+					<b>Godziny otwarcia:</b>
+					<span>Pon: 8:00-16:00</span>
+					<span>Wt: 8:00-16:00</span>
+					<span>Śr: 8:00-16:00</span>
+					<span>Czw: 8:00-16:00</span>
+					<span>Pt: 8:00-16:00</span>
+					<span>Sob: zamknięte</span>
+					<span>Nd: zamknięte</span>
+				</div>
 			</div>
-			<div id="nip-regon">
-				<b>NIP:</b>
-				6181009277<br />
-				<b>REGON:</b>
-				250454919<br />
-				<b>Świadectwo kwalfikacyne:</b>
-				nr 20113/2000
-			</div>
-			<div id="business-time">
-				<b>Godziny otwarcia:</b>
-				Pon: 8:00-16:00<br />
-				Wt: 8:00-16:00<br />
-				Śr: 8:00-16:00<br />
-				Czw: 8:00-16:00<br />
-				Pt: 8:00-16:00<br />
-				Sob: zamknięte<br />
-				Nd: zamknięte<br />
-			</div>
-			<div id="address">
-				<b>Adres:</b>
-				62-840 Koźminek,<br />
-				ul.Nakwasińska 1
-			</div>
-			<div id="phone-number">
-				<b>Telefon:</b>
-				+48 62 763 74 10
-			</div>
-			<div id="e-mail">
-				<b>Email:</b>
-				mireks40<br />@poczta.onet.pl
+			<div id="other">
+				<div id="address">
+					<b>Adres:</b>
+					62-840 Koźminek,<br />
+					ul.Nakwasińska 1
+				</div>
+				<div id="phone-number">
+					<b>Telefon:</b>
+					+48 62 763 74 10
+				</div>
+				<div id="e-mail">
+					<b>Email:</b>
+					mireks40<br />@poczta.onet.pl
+				</div>
 			</div>
 		</div>
-		<a id="credits" href="https://jakubkoralewski.pl/pl" target="_blank">
+		<a
+		 id="credits"
+		 href="https://jakubkoralewski.pl/pl"
+		 target="_blank"
+		>
 			<div id="text">
 				Strona wykonana przez <br />
 				<b>Jakuba Koralewskiego</b>
